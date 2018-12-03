35ad0b630d93ac939e0678b6d02026c65b9c3bcc
Douglas Eleuterio <elettelecom@gmail.com>
2018-12-03 19:06:31 -0200

    Criando operação de Instanciação.

--- src/main/java/com/eleuterio/cursomc/CursomcApplication.java
+++ src/main/java/com/eleuterio/cursomc/CursomcApplication.java
@@ -1,12 +1,29 @@
 package com.eleuterio.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.eleuterio.cursomc.domain.Categoria;
+import com.eleuterio.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
